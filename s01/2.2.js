61b4226f8e0de5a89ebcd44974269f02cb0cbc3c
freddyNgoulou <ngouloujacques19@stud.ase.ro>
2022-10-10 11:03:58 +0300

    Added 2.2.js

---
+++ s01/2.2.js
@@ -0,0 +1,20 @@
+// Check de catte ori apare un chr in str
+// exemplificare for..of
+
+// varianta non-eleganta:
+// function countAppearances(s, c) {
+//     count = 0
+//     for(const elem of s) {
+//         if(elem === c) {
+//             count++
+//         }
+//     }
+//     return count
+// }
+
+// varianta eleganta(exemplificare split()):
+const countAppearances = (s, c) => s.split(c).length - 1
+
+console.log(countAppearances('a little cat', 'a'))
+console.log(countAppearances('', 'a'))
+console.log(countAppearances('a little cat', 'z'))
