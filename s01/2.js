97eb20fa90ed5ed73c5733c392f3d44509bf9a03
freddyNgoulou <ngouloujacques19@stud.ase.ro>
2022-10-10 10:50:08 +0300

    Added s01

--- s01/2.js
+++ s01/2.js
@@ -1,10 +1,28 @@
-function checkDivisor(n, divisor) {
-    if(n % divisor === 0) {
-        return true
-    } else {
-        return false
-    }
-}
+// function checkDivisor(n, divisor) {
+//     if(n % divisor === 0) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// let checkDivisor = function(n, divisor) {
+//     if(n % divisor === 0) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// let checkDivisor = (n, divisor) => {
+//     if(n % divisor === 0) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+let checkDivisor = (n, divisor) => !(n % divisor)
 
 console.log(checkDivisor(6, 3))
 console.log(checkDivisor(6, 5))
