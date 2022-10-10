28bdd5d4fbaa4640a25c4cb4c2a5885b969ae705
freddyNgoulou <ngouloujacques19@stud.ase.ro>
2022-10-10 11:44:19 +0300

    Added 7.js

---
+++ s01/7.js
@@ -0,0 +1,20 @@
+// calc frecventa relativa a unui cuvant in str
+
+function fdist(source) {
+    const results = {}
+    const words = source.split(' ')
+    for(element of words) {
+        if (element in results) {
+            results[element]++
+        } else {
+            results[element] = 1
+        }
+    }
+
+    for(const word in results) {
+        results[word] /= words.length
+    }
+    return results
+}
+
+console.log(fdist('the quick brown fox jumps over the lazy dog'))
